6e5b6d2dc94acfd60563d22e6b5f77ae0b85a92d
Batuhan Y. Yılmaz <batuhan@thebatuhansnetwork.xyz>
2023-06-13 15:57:03 +0300

    Footer
    Look mom I'm a web developer now!

--- my-app/components/footer/index.js
+++ my-app/components/footer/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Link from "next/link";
+
+import styles from "./styles.module.css"
 
 function Footer() {
-    return <div>Footer</div>
+    return <footer className={styles.footer}>
+        Look mom I'm a web developer now!
+    </footer>
 }
 
 export default Footer;
