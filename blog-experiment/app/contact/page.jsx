4b371c5dede77df87d4f49dcab9859fc2c8a2550
Batuhan Y. Yılmaz <70820809+B4tuhanY1lmaz@users.noreply.github.com>
2023-07-20 19:57:56 +0000

    Update the contact page

--- blog-experiment/app/contact/page.jsx
+++ blog-experiment/app/contact/page.jsx
@@ -1,25 +1,46 @@
 import React from "react";
 
-const getText = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users/1")
-    return res.json();
+async function handleSubmit(event) {
+    event.preventDefault();
+
+    const data = {
+        name: String(event.target.name.value),
+        email: String(event.target.email.value),
+        message: String(event.target.message.value),
+    }
+
+    console.log(data); // For now it only console logs the data and this is completely on client side but I'll figure it out.
 }
 
-async function contactPage() {
-
-    const data = await getText();
+function contactPage() {
 
     return <div>
     <div className="flex justify-center mt-40 mb-20">
         <h2 className="text-center text-4xl underline">Get in touch.</h2>
         </div>
-        <div className="flex-col mr-40 ml-40">
-            <div className="justify-center">
-                <h3 className="text-2xl mb-3">Phone</h3>
-                <p>{data.phone}</p>
-                <h3 className="text-2xl mb-3">E Mail</h3>
-                <p>{data.email}</p>
-            </div>
+        <div className="justify-center mx-auto max-w-2xl px-4 my-10">
+            <form onSubmit={handleSubmit}>
+                <div className="w-full flex flex-col">
+                    <label htmlFor="name" >Name</label>
+                    <input type="text" id="name" autoComplete="off" required/>
+                </div>
+                <div className="w-full flex flex-col">
+                    <label htmlFor="email" >E-Mail</label>
+                    <input type="email" id="email" autoComplete="off" required/>
+                </div>
+                <div>
+                    <textarea 
+                    rows={4} 
+                    placeholder="Message comes there"
+                    className="w-full" 
+                    required
+                    id="message"
+                    />
+                </div>
+                <button 
+                type="submit"
+                className="">Submit message!</button>
+            </form>
         </div>
     </div>
 }
