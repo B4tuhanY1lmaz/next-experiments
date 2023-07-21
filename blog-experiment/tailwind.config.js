ae56ee00f9ca172bc635be7243e7fedfdb3dbfc1
Batuhan Y. Yılmaz <batuhan@thebatuhansnetwork.xyz>
2023-07-21 09:48:00 +0300

    Add config files

---
+++ blog-experiment/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./app/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./containers/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
