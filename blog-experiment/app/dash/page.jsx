4bcae512782358e5f908185da15e4efd39accc52
Batuhan Y. Yılmaz <batuhan@thebatuhansnetwork.xyz>
2023-07-26 12:28:14 +0300

    Add dashboard page which does nothing

--- blog-experiment/app/dash/page.jsx
+++ blog-experiment/app/dash/page.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+"use client";
 
-function DashBoard() {
-    return <div>Hello World</div>
-}
-export default DashBoard;
+import React from "react";
+import { useEffect, useState } from "react";
+
+import { UserAuth } from "@/libs/authContext";
+
+const Dashboard = () => {
+    const { user } = UserAuth();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const checkAuthentication = async () => {
+            await new Promise((resolve) => setTimeout(resolve, 50));
+            setLoading(false);
+        };
+        checkAuthentication();
+    }, [user]);
+
+    return (
+        <div>
+            {loading ? (
+                <p>Loading please wait ...</p>
+            ) : user? (
+                <p>Welcome, {user.displayName}!</p>
+            ) : (
+                <p>You must be logged in to view this page</p>
+            )}
+        </div>
+    );
+};
+
+export default Dashboard;
